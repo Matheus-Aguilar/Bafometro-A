5e0a4d7579a197235e80b8ed64c53bedfbbfcb86
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-12-02 16:45:59 -0300

    Added Alcohol Rate Calculations

--- app/src/main/java/android/teste/matheusaguilar/bafometro_a/CalculoActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/bafometro_a/CalculoActivity.java
@@ -2,10 +2,13 @@ package android.teste.matheusaguilar.bafometro_a;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
 public class CalculoActivity extends AppCompatActivity {
+
+    private final int RETURN_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +18,36 @@ public class CalculoActivity extends AppCompatActivity {
 
     public void calcular(View view){
 
+        Intent it = getIntent();
+        Bundle bundle = it.getExtras();
+
+        double peso = bundle.getDouble("peso");
+        String sexo = bundle.getString("sexo");
+        int copos = bundle.getInt("copos");
+        String jejum = bundle.getString("jejum");
+
+        double alcool = copos * 4.8, coef;
+
+        if(jejum.equals("n"))
+            coef = 1.1;
+        else if(sexo.equals("m"))
+            coef = 0.7;
+        else
+            coef = 0.6;
+
+        double taxa = alcool/(peso * coef);
+        String classificacao;
+
+        if(taxa > 0.2)
+            classificacao = "Pessoa Alcoolizada";
+        else
+            classificacao = "Pessoa NÃO Alcoolizada";
+
+        it.putExtra("taxa", taxa);
+        it.putExtra("classificacao", classificacao);
+
+        setResult(RETURN_CODE, it);
+
+        finish();
     }
 }
