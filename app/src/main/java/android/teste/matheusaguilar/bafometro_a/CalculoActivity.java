d476d46b65479dea80720e26a1cfc3f9b31087bb
Matheus-Aguilar <matheusdeoliveira.ti@gmail.com>
2020-12-02 15:42:45 -0300

    Added Screens

--- app/src/main/java/android/teste/matheusaguilar/bafometro_a/CalculoActivity.java
+++ app/src/main/java/android/teste/matheusaguilar/bafometro_a/CalculoActivity.java
@@ -5,15 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class CalculoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calculo);
     }
 
-    public void enviarDados(View view){
+    public void calcular(View view){
 
     }
 }
